c093006ad8508655afa8d7135dba9bd04c9dab1f
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2023-10-22 09:50:31 +0700

    update formik for chakraUI

--- src/components/Forms/LoginForm.tsx
+++ src/components/Forms/LoginForm.tsx
@@ -11,6 +11,8 @@ import {
   Divider,
   AbsoluteCenter,
 } from '@chakra-ui/react';
+import { Formik } from 'formik';
+import { InputControl } from 'formik-chakra-ui';
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
 import React from 'react';
 import SVGIcon from '../Icons/SVGIcon';
@@ -18,6 +20,7 @@ import GoogleIcon from '@/assets/svgs/google-icon.svg';
 import BaseForm from './BaseForm';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import * as Yup from 'yup';
 
 export default function LoginForm() {
   const router = useRouter();
@@ -26,6 +29,20 @@ export default function LoginForm() {
     email: '',
     password: '',
   });
+  const initialValues = {
+    firstName: '',
+    email: '',
+    password: '',
+  };
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string().required(),
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string().required(),
+  });
+  const onSubmit = () => {
+    console.log('submit');
+  };
+
   return (
     <BaseForm>
       <FormControl>
@@ -33,45 +50,55 @@ export default function LoginForm() {
         <p className="text-secondary font-bold text-[15px] my-[20px]">
           Enter your email and password to sign in
         </p>
-        {/* Email */}
-        <FormLabel>Email</FormLabel>
-        <Input placeholder="Your email" type="email" />
-        {/* Password */}
-        <FormLabel>Password</FormLabel>
-        <InputGroup>
-          <Input
-            placeholder="Your password"
-            type={isVisible ? 'text' : 'password'}
-          />
-          <InputRightElement onClick={() => setIsVisible(!isVisible)}>
-            {isVisible ? (
-              <ViewIcon
-                color="gray.400"
-                boxSize={5}
-                className="hover:opacity-50 hover:cursor-pointer"
-              />
-            ) : (
-              <ViewOffIcon
-                color="gray.400"
-                boxSize={5}
-                className="hover:opacity-50 hover:cursor-pointer"
-              />
-            )}
-          </InputRightElement>
-        </InputGroup>
-        <FormHelperText>
-          <Link className="text-primary" href="/auth/forget-password">
-            Forgot password ?
-          </Link>
-        </FormHelperText>
-        {/* Sign In Button */}
-        <Button
-          className="w-full mt-[20px]"
-          colorScheme="teal"
-          variant="solid"
-          onClick={() => router.push('/')}>
-          Sign in
-        </Button>
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validationSchema={SignupSchema}>
+          {({ handleSubmit, values, errors }) => (
+            <div>
+              <InputControl name="firstName" label="First Name" />
+              {/* Email */}
+              <FormLabel>Email</FormLabel>
+              <Input placeholder="Your email" type="email" />
+              {/* Password */}
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input
+                  placeholder="Your password"
+                  type={isVisible ? 'text' : 'password'}
+                />
+                <InputRightElement onClick={() => setIsVisible(!isVisible)}>
+                  {isVisible ? (
+                    <ViewIcon
+                      color="gray.400"
+                      boxSize={5}
+                      className="hover:opacity-50 hover:cursor-pointer"
+                    />
+                  ) : (
+                    <ViewOffIcon
+                      color="gray.400"
+                      boxSize={5}
+                      className="hover:opacity-50 hover:cursor-pointer"
+                    />
+                  )}
+                </InputRightElement>
+              </InputGroup>
+              <FormHelperText>
+                <Link className="text-primary" href="/auth/forget-password">
+                  Forgot password ?
+                </Link>
+              </FormHelperText>
+              {/* Sign In Button */}
+              <Button
+                className="w-full mt-[20px]"
+                colorScheme="teal"
+                variant="solid"
+                onClick={() => router.push('/')}>
+                Sign in
+              </Button>
+            </div>
+          )}
+        </Formik>
         <Box position="relative" padding="5">
           <Divider />
           <AbsoluteCenter
