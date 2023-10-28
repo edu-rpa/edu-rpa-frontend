bd11470c56a574c739ba876742b7ab46c2e7a533
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2023-10-28 13:13:06 +0700

    refactor bpmn-parser test

--- __test__/bpmn-parser.utill.test.ts
+++ __test__/bpmn-parser.utill.test.ts
@@ -1,16 +1,27 @@
-import { Bpmn2RbtParser } from "@/utils/bpmn-parser/bpmn-2-rbt-parser.utils"
-import { BpmnParser } from "@/utils/bpmn-parser/bpmn-parser.util"
-import { BpmnParseError } from "@/utils/bpmn-parser/error"
+import { BpmnParser } from '@/utils/bpmn-parser/bpmn-parser.util';
+import { BpmnParseError } from '@/utils/bpmn-parser/error';
+import { promises as fsPromises } from 'fs';
+import { dirname } from 'path';
 
+async function parseAndWriteBpmnToFile(inputPath: string, outputPath: string) {
+  const process = new BpmnParser().parse(inputPath);
+  await fsPromises.mkdir(dirname(outputPath), { recursive: true });
+  await fsPromises.writeFile(outputPath, JSON.stringify(process, null, 2));
+  console.log(BpmnParseError);
+}
 
-describe("BPMN Test", () => {
-    it("Test", () => {
-        expect(() => new BpmnParser().parse("__test__/bpmn/1.xml")).toThrow(BpmnParseError)
-    })
+describe('BPMN Test', () => {
+  it('Test 1', async () => {
+    await parseAndWriteBpmnToFile(
+      '__test__/bpmn/1.xml',
+      '__test__/output/1.json'
+    );
+  });
 
-    it("Test", () => {
-        let process = new BpmnParser().parse("__test__/bpmn/2.xml")
-        console.log(process)
-    })
-
-})
+  it('Test 2', async () => {
+    await parseAndWriteBpmnToFile(
+      '__test__/bpmn/2.xml',
+      '__test__/output/2.json'
+    );
+  });
+});
