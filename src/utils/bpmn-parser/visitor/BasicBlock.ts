b0bf84b1863666924cd3ce7e50d239ca7ee64dcf
David Huynh <78911364+02david20@users.noreply.github.com>
2023-10-30 08:20:58 +0700

    Refactor code by visitor pattern

--- src/utils/bpmn-parser/visitor/BasicBlock.ts
+++ src/utils/bpmn-parser/visitor/BasicBlock.ts
@@ -1,19 +1,39 @@
+import { log } from "console";
 import { BpmnNode } from "../model/bpmn";
 
 export class Block {}
 
 export class Sequence {
-    constructor(
-        public block: (BpmnNode|Branch)[] = []
-    ) {}
+  constructor(
+    public block: (BpmnNode | Branch)[] = [],
+    public scope?: Branch
+  ) {}
+  public toString (indent: number): string {
+    return this.block.map((b) => b.toString(indent)).join("\n");
+  };
 }
 
 export class Branch extends Block {
-    constructor(
-        public start: string,
-        public braches : Block[] = [],
-    ) {
-        super()
+  constructor(public start: string, public braches: Sequence[] = []) {
+    super();
+  }
+  public toString (indent: number): string {
+    let branchCode = "\n";
+    for(let i = 0 ; i < this.braches.length; i++) {
+        if(i == 0) {
+            branchCode += genIndent(indent, `IF: ${this.start}\n`);
+        }else if(i == this.braches.length-1) {
+            branchCode += genIndent(indent, "ELSE:") + "\n";
+        }else {
+            branchCode += genIndent(indent, "ELSE IF:") + "\n";
+        }
+        branchCode += this.braches[i].toString(indent+1) + "\n"
     }
+    return branchCode;
+  };
 }
 
+export function genIndent(indentLevel: number, line: string): string {
+  let indent = "\t".repeat(indentLevel);
+  return indent + line
+}
