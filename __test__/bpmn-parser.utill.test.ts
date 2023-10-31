1b827f082ce45f821abdde8696533a2a60b8281b
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2023-10-31 16:15:49 +0700

    commit __test__

--- __test__/bpmn-parser.utill.test.ts
+++ __test__/bpmn-parser.utill.test.ts
@@ -1,54 +1,65 @@
-import { Bpmn2RbtParser } from "@/utils/bpmn-parser/bpmn-2-rbt-parser.utils";
-import { BpmnParser } from "@/utils/bpmn-parser/bpmn-parser.util";
-import { BpmnParseError } from "@/utils/bpmn-parser/error";
-import { log } from "console";
-var fs = require("fs");
+import { Bpmn2RbtParser } from '@/utils/bpmn-parser/bpmn-2-rbt-parser.utils';
+import { BpmnParser } from '@/utils/bpmn-parser/bpmn-parser.util';
+import { BpmnParseError } from '@/utils/bpmn-parser/error';
+var fs = require('fs');
 var options = { ignoreComment: true, alwaysChildren: true };
 
-describe("BPMN Test", () => {
-  it("Test", () => {
-    expect(() => new BpmnParser().parse("__test__/bpmn/1.xml")).toThrow(
+describe('BPMN Test', () => {
+  it('Test', () => {
+    expect(() => new BpmnParser().parse('__test__/bpmn/1.xml')).toThrow(
       BpmnParseError
     );
   });
 
-  it("Normalize form Split-Join", () => {
-    let testcase = 2
-    let process = new BpmnParser().parse(`__test__/bpmn/${testcase}.xml`);   
-    let raw= fs.readFileSync(`__test__/bpmn/expected/${testcase}.json`, "utf8")
-    let expected = JSON.parse(raw)
-    expect(JSON.stringify(process, null, 2)).toEqual(JSON.stringify(expected, null, 2))
+  it('Normalize form Split-Join', () => {
+    let testcase = 2;
+    let process = new BpmnParser().parse(`__test__/bpmn/${testcase}.xml`);
+    let raw = fs.readFileSync(
+      `__test__/bpmn/expected/${testcase}.json`,
+      'utf8'
+    );
+    let expected = JSON.parse(raw);
+    expect(JSON.stringify(process, null, 2)).toEqual(
+      JSON.stringify(expected, null, 2)
+    );
   });
 
-  it("Nested Branching", () => {
-    let testcase = 3
-    let process = new BpmnParser().parse(`__test__/bpmn/${testcase}.xml`);   
-    let raw= fs.readFileSync(`__test__/bpmn/expected/${testcase}.json`, "utf8");
-    let expected = JSON.parse(raw)
-    expect(JSON.stringify(process, null, 2)).toEqual(JSON.stringify(expected, null, 2))
+  it('Nested Branching', () => {
+    let testcase = 3;
+    let process = new BpmnParser().parse(`__test__/bpmn/${testcase}.xml`);
+    let raw = fs.readFileSync(
+      `__test__/bpmn/expected/${testcase}.json`,
+      'utf8'
+    );
+    let expected = JSON.parse(raw);
+    expect(JSON.stringify(process, null, 2)).toEqual(
+      JSON.stringify(expected, null, 2)
+    );
   });
 
-  it("Have Loop", () => {
-    const t = () => new BpmnParser().parse("__test__/bpmn/4.xml");
+  it('Have Loop', () => {
+    const t = () => new BpmnParser().parse('__test__/bpmn/4.xml');
     expect(t).toThrow(BpmnParseError);
-    expect(t).toThrow("Detected Loop in Process - Unsupported");
+    expect(t).toThrow('Detected Loop in Process - Unsupported');
   });
 
-  it("Not Normalize Form Split-Join", () => {
-    let process = new BpmnParser().parse("__test__/bpmn/5.xml");
+  it('Not Normalize Form Split-Join', () => {
+    let process = new BpmnParser().parse('__test__/bpmn/5.xml');
   });
 
-  it("Middle Break Or Return", () => {
-    let t = () => new BpmnParser().parse("__test__/bpmn/6.xml");
-    expect(t).toThrow(TypeError)
+  it('Middle Break Or Return', () => {
+    let t = () => new BpmnParser().parse('__test__/bpmn/6.xml');
+    expect(t).toThrow(TypeError);
   });
 
-  it("Not Normalize 1", () => {
-    let process = new BpmnParser().parse("__test__/bpmn/7.xml");
-    let raw= fs.readFileSync("__test__/bpmn/expected/7.json", "utf8")
-    let expected = JSON.parse(raw)
-    expect(JSON.stringify(process, null, 2)).toEqual(JSON.stringify(expected, null, 2))
+  it('Not Normalize 1', () => {
+    let process = new BpmnParser().parse('__test__/bpmn/7.xml');
+    let raw = fs.readFileSync('__test__/bpmn/expected/7.json', 'utf8');
+    let expected = JSON.parse(raw);
+    expect(JSON.stringify(process, null, 2)).toEqual(
+      JSON.stringify(expected, null, 2)
+    );
   });
 
-  it("Have Many Endpoint => Return", () => {})
+  it('Have Many Endpoint => Return', () => {});
 });
