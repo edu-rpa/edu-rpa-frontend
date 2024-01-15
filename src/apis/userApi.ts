cc7978d0fca1e66cb21deda46f87c53aedc71417
Quang Khánh <khanhnguyenmcu@gmail.com>
2024-01-16 01:03:18 +0700

    Connect update profile API

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -1,3 +1,4 @@
+import { UpdateProfileDto } from '@/dtos/userDto';
 import apiBase from './config';
 
 const getMe = async () => {
@@ -8,8 +9,28 @@ const getMe = async () => {
     });
 };
 
+const updateProfile = async (payload: UpdateProfileDto) => {
+  return await apiBase
+    .put(`${process.env.NEXT_PUBLIC_DEV_API}/users/me`, payload)
+    .then((res: any) => {
+      return res.data;
+    });
+};
+
+const uploadAvatar = async (file: File) => {
+  const formData = new FormData();
+  formData.append('file', file);
+  return await apiBase
+    .post(`${process.env.NEXT_PUBLIC_DEV_API}/users/me/avatar`, formData)
+    .then((res: any) => {
+      return res.data;
+    });
+};
+
 const userApi = {
   getMe,
+  updateProfile,
+  uploadAvatar,
 };
 
 export default userApi;
--- src/redux/selector.tsx
+++ src/redux/selector.tsx
@@ -3,3 +3,4 @@ import { RootState } from './store';
 export const authSelector = (state: RootState) => state.auth;
 export const homeSelector = (state: RootState) => state.home;
 export const bpmnSelector = (state: RootState) => state.bpmn;
+export const userSelector = (state: RootState) => state.user;
