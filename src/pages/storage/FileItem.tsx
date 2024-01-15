8ed02bc298c4ee88c8874968600268e2f5538c65
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2024-01-15 22:01:01 +0700

    fix build

--- src/pages/storage/FileItem.tsx
+++ src/pages/storage/FileItem.tsx
@@ -12,10 +12,15 @@ interface FileItemProps {
   isLoading?: boolean;
 }
 
-const FileItem: React.FC<FileItemProps> = ({ name, onClick, onClickDelete, isLoading }) => {
+const FileItem: React.FC<FileItemProps> = ({
+  name,
+  onClick,
+  onClickDelete,
+  isLoading,
+}) => {
   const [isHovered, setIsHovered] = useState(false);
 
-  const isDirectory = name.endsWith('/');
+  const isDirectory = name && name.endsWith('/');
 
   const handleMouseEnter = () => {
     setIsHovered(true);
@@ -27,31 +32,38 @@ const FileItem: React.FC<FileItemProps> = ({ name, onClick, onClickDelete, isLoa
 
   return (
     <div
-      className={`flex flex-col justify-center items-center cursor-pointer ${isHovered ? 'bg-gray-300' : ''} ${isLoading ? 'opacity-50' : ''}`}
+      className={`flex flex-col justify-center items-center cursor-pointer ${
+        isHovered ? 'bg-gray-300' : ''
+      } ${isLoading ? 'opacity-50' : ''}`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
       onClick={() => onClick(name)}
-      style={{ position: 'relative' }}
-    >
-      <div className="flex justify-center items-center">{
-        isDirectory
-          ? <FolderOpenIcon sx={{ fontSize: 60 }} />
-          : isHovered ? <CloudDownloadIcon sx={{ fontSize: 60 }} /> : <ArticleIcon sx={{ fontSize: 60 }} />
-      }</div>
+      style={{ position: 'relative' }}>
+      <div className="flex justify-center items-center">
+        {isDirectory ? (
+          <FolderOpenIcon sx={{ fontSize: 60 }} />
+        ) : isHovered ? (
+          <CloudDownloadIcon sx={{ fontSize: 60 }} />
+        ) : (
+          <ArticleIcon sx={{ fontSize: 60 }} />
+        )}
+      </div>
       <div className="text-center">{name}</div>
-      {isHovered && <IconButton
-        aria-label="delete"
-        size="sm"
-        style={{ position: 'absolute', top: 0, right: 0 }}
-        colorScheme='red'
-        isRound
-        variant='outline'
-        icon={<ClearIcon />}
-        onClick={(e) => {
-          e.stopPropagation();
-          onClickDelete(name);
-        }}
-      />}
+      {isHovered && (
+        <IconButton
+          aria-label="delete"
+          size="sm"
+          style={{ position: 'absolute', top: 0, right: 0 }}
+          colorScheme="red"
+          isRound
+          variant="outline"
+          icon={<ClearIcon />}
+          onClick={(e) => {
+            e.stopPropagation();
+            onClickDelete(name);
+          }}
+        />
+      )}
     </div>
   );
 };
