48ab5602b808777b4dba19e6164dadaddd9df183
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2024-04-18 22:48:53 +0700

    feat: update stream log from cloudwatch

--- src/pages/robot/detail/[id].tsx
+++ src/pages/robot/detail/[id].tsx
@@ -14,15 +14,6 @@ const RobotCode = () => {
   const router = useRouter();
   const params = useParams();
 
-  const { data: robotDetail, isLoading } = useQuery({
-    queryKey: [QUERY_KEY.PROCESS_DETAIL],
-    queryFn: () => robotApi.getRobotByID(params.id as string),
-  });
-
-  if (isLoading) {
-    return <LoadingIndicator />;
-  }
-
   return (
     <Container maxW="container.xl" className="bg-white h-[100vh]">
       <Box className="flex justify-between items-center w-90 m-auto">
@@ -58,9 +49,6 @@ const RobotCode = () => {
           Robot Information:
         </Text>
         <Text>ID: {params && params.id} </Text>
-      </Box>
-      <Box className="w-90 m-auto">
-        <CodeViewer code={robotDetail.code} language="robot" />
       </Box>
     </Container>
   );
