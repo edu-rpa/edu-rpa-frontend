28048db394b5894fbf9c9edff72864da25f3ee4c
Nguyễn Đức An <an.nguyenduc1406@hcmut.edu.vn>
2024-04-29 16:34:50 +0700

    feat: change api endpoint for getLogRobotDetail

--- src/apis/robotReportApi.ts
+++ src/apis/robotReportApi.ts
@@ -7,7 +7,7 @@ const getRobotLogDetail = async (
 ) => {
   return await apiBase
     .get(
-      `${process.env.NEXT_PUBLIC_DEV_API}/robot-report/run-detail/${streamID}/${processID}/${version}`
+      `${process.env.NEXT_PUBLIC_DEV_API}/robot-report/detail?streamID=${streamID}&processID=${processID}&version=${version}`
     )
     .then((res: any) => {
       return res.data;
